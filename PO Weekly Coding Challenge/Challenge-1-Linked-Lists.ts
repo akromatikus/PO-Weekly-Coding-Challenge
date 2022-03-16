0883c76135046ad38a268032032e82a3163e28cf
akromatikus <31263630+akromatikus@users.noreply.github.com>
2022-03-16 16:37:21 -0500

    latest

--- PO Weekly Coding Challenge/Challenge-1-Linked-Lists.ts
+++ PO Weekly Coding Challenge/Challenge-1-Linked-Lists.ts
@@ -1,7 +1,58 @@
 
-let listSize = 10
+interface LLNode{
+    value: any
+    next: LLNode | null
+}
+
+interface LLMethods{
+    insert(value: LLNode): void
+    traverse(): void
+    getValue(): LLNode
+}
+
+class LLConstructor implements LLMethods{
+    //private LL: LLNode[] 
+    private head: LLNode
+    private currentNode: LLNode 
+
+    constructor(){
+        //this.LL = []
+        this.head = this.currentNode = null
+    }
+
+    insert(value){
+        
+        //if the head is empty (no LL yet)
+        if (!this.head){
+            this.head.value = value
+            this.head.next = this.currentNode
+        }
+        //else the head already has values so...
+        else{
+            //this.currentNode.next = {value:value, next:null} 
+            this.currentNode = {value:value, next:null}
+        }
+
+    }
+
+    traverse(): void {
+        
+    }
+}
+
+
+
+let Node1: LLNode = {value:1, next: null}
+
+
+
+
+
+
+
+
 
 //generate linked lists
-for (let iter = 0; iter < listSize; iter++ ){
+// for (let iter = 0; iter < listSize; iter++ ){
 
-}
+// }
