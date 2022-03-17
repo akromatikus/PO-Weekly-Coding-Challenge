ad0e4f0563e91d5327550ad740009ebbc6ce49d5
akromatikus <31263630+akromatikus@users.noreply.github.com>
2022-03-17 11:11:38 -0500

    finished

--- PO Weekly Coding Challenge/Challenge-1-Linked-Lists.ts
+++ PO Weekly Coding Challenge/Challenge-1-Linked-Lists.ts
@@ -1,16 +1,24 @@
+// 1) Given two singly linked lists that intersect at some point, find the intersecting node. Assume the lists are non-cyclical.
+
+// For example, given A = 3 ➔ 7 ➔ 8 ➔ 10 and B = 99 ➔ 1 ➔ 8 ➔ 10, return the node with value 8. In this example, assume nodes with the same value are the exact same node objects.
+
+// Do this in 0( m + n) time (where m and n are the lengths of the lists) and constant space.
+
 
 interface LLNode{
-    value: any
+    value: any 
     next: LLNode | null
 }
 
-interface LLMethods{
+interface linkedList{
     NodeAdd(value: any): void
     NodesPrint(): void
+    getLength(): number
+    head: LLNode | null
 }
 
-class LLConstructor implements LLMethods{ 
-    private head: LLNode | null
+class LLConstructor implements linkedList{ 
+    head: LLNode | null
     private nodeCurrent: LLNode | null
     private length: number
 
@@ -20,12 +28,13 @@ class LLConstructor implements LLMethods{
     }
 
     NodeAdd(value: any){
-        const node = {value:value,next:null}
+        const node = {value:value, next:null}
         
         //if the head is empty (no LL yet). !Only applies to first insert iteration!
         if (!this.head){
             this.head = node 
-            console.log("head initialized to: ", this.head)
+            
+            //console.log("head initialized to: ", this.head)
         }
         
         //else the head already has values so...
@@ -33,52 +42,80 @@ class LLConstructor implements LLMethods{
 
             //make the current node the head node. !Applies to all but first iteration!
             this.nodeCurrent = this.head
-            console.log("nodeCurrent assigned to head, head is: ", this.head)
+
+            //console.log("nodeCurrent assigned to head, head is: ", this.head)
             
             //while the current node has an object for its next property.
-            // !Applies to all iterations except first and second!
+            // !Applies to all iterations except first!
             while (this.nodeCurrent.next){
-                console.log("while loop started")
+                
+                //console.log("while loop started")
                 //assign it to that object
                 this.nodeCurrent = this.nodeCurrent.next
             }
 
             //make next another node object with both the value and next property
-            console.log("The nodeCurrent is now:", this.nodeCurrent, "its next property will be changed to: ", node, '\n')
+            //console.log("The nodeCurrent is now:", this.nodeCurrent, "its next property will be changed to: ", node, '\n')
             this.nodeCurrent.next = node
         }
 
         this.length++
-
     }
 
     NodesPrint(): void {
         let nodeCurrent = this.head
+
         while (nodeCurrent){
             console.log(nodeCurrent.value)
             nodeCurrent = nodeCurrent.next
         }
+
+        console.log("")
     }
+
+    getLength(): number {
+        return this.length
+    }
+}
+
+function LLfindIntersect(LLone: linkedList, LLtwo: linkedList){
+    let LLOneCurrent = LLone.head
+    let LLTwoCurrent = LLtwo.head
+    
+    for (let iter = 0; iter < Math.min(LLone.getLength(), LLtwo.getLength()); iter++){
+        if (LLOneCurrent!.value === LLTwoCurrent!.value) {
+            
+            let result = `The first LL node is ${JSON.stringify(LLOneCurrent)}. The second LL node is ${JSON.stringify(LLTwoCurrent)}. The value they share is ${LLOneCurrent!.value}`
+            return result
+        }
+        else {
+            LLOneCurrent = LLOneCurrent!.next
+            LLTwoCurrent = LLTwoCurrent!.next
+        }
+    }
+
+    //only returns if no intersection was found
+    return "No intersection found."
 }
 
 
 
-let LLone: LLMethods = new LLConstructor
+let LLone: linkedList = new LLConstructor
 LLone.NodeAdd(1)
 LLone.NodeAdd(2)
 LLone.NodeAdd(3)
 LLone.NodeAdd(4)
-LLone.NodesPrint()
 
+let LLtwo: linkedList = new LLConstructor
+LLtwo.NodeAdd(28)
+LLtwo.NodeAdd(13)
+LLtwo.NodeAdd(3)
+LLtwo.NodeAdd(8)
+LLtwo.NodeAdd(6)
+LLtwo.NodeAdd(15)
 
+// LLone.NodesPrint()
+// LLtwo.NodesPrint()
 
-
-
-
-
-
-
-//generate linked lists
-// for (let iter = 0; iter < listSize; iter++ ){
-
-// }
+let intersectNodes = LLfindIntersect(LLone, LLtwo)
+console.log(intersectNodes)
